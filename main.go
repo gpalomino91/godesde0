20a7161791b2a1337c9ab18b8b80de0d086bf0ce
Gonzalo <104773712+gpalomino91@users.noreply.github.com>
2023-12-18 18:53:05 -0600

    Siguiente Commit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/gpalomino91/godesde0/variables"
 )
@@ -10,4 +11,18 @@ func main() {
 	estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)
+	if os := runtime.GOOS; os == "Linux." || os == "darwin" {
+		fmt.Println("Esto no es Windows")
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
